f173bd3bccccca65c18d4b1a8db946310d5f27b9
grunners <grunners22@hotmail.com>
2018-01-09 14:57:01 +0000

    added basic icon support

--- js/models/forecast_model.js
+++ js/models/forecast_model.js
@@ -4,21 +4,36 @@ var Forecast = function(info) {
 	this.longitude = info.longitude;
 }
 
+function Icon(value) {
+	switch(value) {
+		case "rain": 
+			return "<i class='fa fa-tint' aria-hidden='true'></i>";
+			break;
+		case "partly-cloudy-day":
+			return "<i class='fa fa-cloud' aria-hidden='true'></i>";
+			break;
+		default:
+			return "<i class='fa fa-sun-o' aria-hidden='true'></i>";
+			break;
+	}
+
+}
+
 Forecast.prototype.loadForecast = function() {
 	var self = this;
 	const ds_key = "f5fcf916bad24f6c6c3a002daffc95dc";
-	//console.log(latitude + "," + longitude + " " + ds_key);
 	var lat_long = this.latitude + "," + this.longitude;
 	//use cors proxy - build locally when ready
-	var weather_url = "http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + ds_key + "/" + lat_long;
+	var weather_url = "http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + ds_key + "/" + lat_long + "?units=si";
 
 	var request = $.ajax({
        	url: weather_url,
         type: 'GET',
 		success: function(result) {
 			$('#loader').hide();
-			self.summary = result.currently.summary;
-			console.log(result.currently.summary);
+			self.summary = Math.round(result.currently.temperature) + "&deg;C";
+			self.icon = Icon(result.currently.icon);
+			console.log(result);
 		},
         //invalid
         error: function() {
--- js/views/forecast_view.js
+++ js/views/forecast_view.js
@@ -4,12 +4,14 @@ var ForecastView = function(forecast) {
 	this.forecast = forecast;
 	// set the div with class forecast as domain of this view
 	this.$el = $(".forecast");
+	this.$el = $(".icon");
 }
 
 ForecastView.prototype = {
 	forecastTemplate: function(forecast) {
 		var html = $("<div></div>");
 		html.append("<p class='description'>" + forecast.summary + "</p>");
+		html.append("<p class='icon'>" + forecast.icon + "</p>");
 		return(html);
 	},
 	render: function() {
